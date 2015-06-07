e3f795daf0af240d812a3c48853d8adcbf5c3264
Marco Jonker <marco.a.jonker@gmail.com>
2015-06-07 00:55:05 -0700

    iterator async.timesSeries works, but displacing page isnt working yet -- routes/instagram.js

--- routes/instagram.js
+++ routes/instagram.js
@@ -31,11 +31,24 @@ module.exports = (function() {
             },
             function(trends, done) {
                 async.map(trends, function(trend, next) {
-                    ig.tag_media_recent(trend.name, {count: 33}, function(err, medias, pagination, remaining, limit) {
-                        if (err) inner_next(err);
-                        //TODO get last 1000. order by likes.
-                        next(null, medias);
+                    var max_page;
+                    var init = { count: 33 };
+                    var post_init = { count: 33, max_tag_id: max_page };
+                    async.timesSeries(5, function(n, inner_next) {
+                        ig.tag_media_recent(trend.name, ( n === 0 ? init: post_init), function(err, medias, pagination, remaining, limit) {
+                            if (err) inner_next(err);
+                            //TODO get last 1000. order by likes.
+                            // getting the 5 iterations of the same items.
+                            // cant displace by next_max_id like expected
+                            max_page = pagination.next_max_id;
+                            console.log(trend.name, max_page, pagination);
+                            inner_next(null, medias);
+                        });
+                    }, function(err, results) {
+                        if (err) return next(err);
+                        next(null, results);
                     });
+
                 }, function(err, results) {
                     if (err) return console.error(err);
                     done(null, results);
