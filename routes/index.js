11162bc16be00efacf2e7dfaca5e556adc1b5bc0
Marco Jonker <marco.a.jonker@gmail.com>
2015-06-08 21:19:30 -0700

    made twitter trends and instagram pictures happen on an interval. index.js

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,36 @@ var express = require('express'),
     instagram = require('./instagram');
 var router = express.Router();
 
-var world_trends;
+var world_trends, ig_pics;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    twitter.trends(function(err, trends) {
-        if (err) console.error(err);
-        world_trends = trends;
-        // console.log(trends);
-            instagram.pictures(trends, function(err, pictures) {
-                if (err) console.error(err);
-                res.send(pictures);
-            });
-    });
+    res.send(ig_pics);
 });
+
+twitter.trends(function(err, trends) {
+    if (err) console.error(err);
+    world_trends = trends;
+    instagram.pictures(world_trends, function(err, pictures) {
+        if (err) console.error(err);
+        ig_pics = pictures;
+        console.log(ig_pics);
+    });
+    setInterval(function() {
+        instagram.pictures(trends, function(err, pictures) {
+            if (err) console.error(err);
+            ig_pics = pictures;
+            console.log(ig_pics);
+        });
+    }, 90000);
+    setInterval(function() {
+        twitter.trends(function(err, trends) {
+            if (err) console.error(err);
+            world_trends = trends;
+            console.log(world_trends);
+        });
+    }, 100000);
+});
+
+
 
 module.exports = router;
